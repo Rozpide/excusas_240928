a1c3a9c821f4226556d252dec7d4cc48af4af856
Rozpi <100348581+Rozpide@users.noreply.github.com>
2024-10-02 18:51:53 +0000

    base

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
+import "style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let quien = ["Mi gato ", "Mi madre ", "Mi hijo ", "Mi vecino "];
+  let accion = ["se comio ", " ha perdido ", " ha roto ", " ha escondido "];
+  let que = [" mis ejercicios ", "mi telefono ", " mi tarea ", " mi libro "];
+  let cuando = [
+    " mientras paseaba ",
+    " cuando estaba dormido",
+    " porque no le gustaba",
+    " cuando estaba jugando",
+    " porque estaba aburrido"
+  ];
+
+  function generarUnaExcusa() {
+    let quienIndex = Math.floor(Math.random() * quien.length);
+    let accionIndex = Math.floor(Math.random() * accion.length);
+    let queIndex = Math.floor(Math.random() * que.length);
+    let cuandoIndex = Math.floor(Math.random() * cuando.length);
+
+    let excusa =
+      quien[quienIndex] +
+      accion[accionIndex] +
+      que[queIndex] +
+      cuando[cuandoIndex];
+    document.getElementById("excusa").innerHTML = excusa;
+  }
   //write your code here
-  console.log("Hello Rigo from the console!");
+  generarUnaExcusa();
 };
